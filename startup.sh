bf6db4c49ca9e744cf4ff1641a3b3726b52951bb
Logan Walker <loganaw@umich.edu>
2023-06-21 11:34:01 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is run as the docker entrypoint as root before
+# launching daemons and the user startup below
+
 /usr/sbin/sshd
 
 su ubuntu -c"/bin/bash /home/ubuntu/startup2.sh"
